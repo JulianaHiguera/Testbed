f5d7cfc8fee90835a6853c3834cc4dc27d7e1fcc
JulianaHiguera <zhiguera@unboque.edu.co>
2023-05-17 23:05:19 -0500

    Prueba creeación contenedor docker_3.2

--- prueba.php
+++ prueba.php
@@ -7,14 +7,17 @@
 
 <body>
     <button id="create-container-btn">Crear Contenedor</button>
-
     <script>
         document.getElementById('create-container-btn').addEventListener('click', function () {
             var xhr = new XMLHttpRequest();
             xhr.open('GET', 'create-container.php', true);
             xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    alert('Se ha creado el contenedor con ID: ' + xhr.responseText);
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        alert('Se ha creado el contenedor con ID: ' + xhr.responseText);
+                    } else {
+                        alert('Error al crear el contenedor: ' + xhr.status);
+                    }
                 }
             };
             xhr.send();
