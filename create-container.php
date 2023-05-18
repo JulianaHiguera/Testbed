170ac1e6e9bf161d9c48f1bb951c6cdf3f2ea9cf
JulianaHiguera <zhiguera@unboque.edu.co>
2023-05-17 23:18:52 -0500

    Prueba creeación contenedor docker_3.3

--- create-container.php
+++ create-container.php
@@ -1,16 +1,56 @@
 <?php
-require 'vendor/autoload.php'; // Carga la biblioteca Docker PHP
+// Datos de configuración de Docker
+$dockerHost = 'unix:///var/run/docker.sock'; // o 'tcp://localhost:2375' para conexión a través de TCP
+$dockerEndpoint = $dockerHost . '/v1.41/containers/create';
 
-use Docker\Docker;
+// Configuración del nuevo contenedor
+$containerName = 'mi_contenedor';
+$image = 'imagen_docker:tag';
 
-// Crear una instancia de Docker
-$docker = new Docker();
+// Crear solicitud para crear el contenedor
+$data = array(
+    'Image' => $image,
+    'name' => $containerName
+);
 
-// Crear un nuevo contenedor
-$container = $docker->containerCreate(['Image' => 'ubuntu']);
+$options = array(
+    CURLOPT_URL => $dockerEndpoint,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => json_encode($data),
+    CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+    CURLOPT_RETURNTRANSFER => true
+);
+
+// Enviar la solicitud a la API de Docker
+$ch = curl_init();
+curl_setopt_array($ch, $options);
+$response = curl_exec($ch);
+curl_close($ch);
+
+// Decodificar la respuesta de la API de Docker
+$containerInfo = json_decode($response, true);
 
 // Obtener el ID del contenedor creado
-$containerId = $container['Id'];
+$containerId = $containerInfo['Id'];
 
-// Devolver el ID del contenedor
-echo $containerId;
+// Iniciar el contenedor
+$dockerStartEndpoint = $dockerHost . '/v1.41/containers/' . $containerId . '/start';
+
+$options = array(
+    CURLOPT_URL => $dockerStartEndpoint,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true
+);
+
+$ch = curl_init();
+curl_setopt_array($ch, $options);
+$response = curl_exec($ch);
+curl_close($ch);
+
+// Verificar el resultado de la solicitud
+if ($response === false) {
+    echo 'Error al crear el contenedor.';
+} else {
+    echo 'Contenedor creado exitosamente.';
+}
+?>
