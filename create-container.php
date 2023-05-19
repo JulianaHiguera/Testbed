a4fadf8f860abdf8dceb560d74ffba5b653f60d8
JulianaHiguera <zhiguera@unboque.edu.co>
2023-05-19 14:08:36 -0500

    cambio

--- create-container.php
+++ create-container.php
@@ -1,7 +1,9 @@
 <?php
+$sistemaOperativo = $_POST['sistema-operativo'];
+$duracion = $_POST['duracion'];
 
 $password = "juli";
-$command = "echo '$password' | sudo -S ./crar_imagen.sh";
+$command = "echo '$password' | sudo -S ./crar_imagen.sh '$sistemaOperativo'";
 
 $output = shell_exec($command);
 echo $output;
