829d8064eace815b2032ef07e936f3e88d57cad5
JulianaHiguera <zhiguera@unboque.edu.co>
2023-05-19 14:02:02 -0500

    Unificado

--- create-container.php
+++ create-container.php
@@ -1,34 +1,8 @@
 <?php
 
-require 'vendor/autoload.php';
+$password = "juli";
+$command = "echo '$password' | sudo -S ./crar_imagen.sh";
 
-use Docker\Docker;
-use Docker\API\Model\ContainersCreatePostBody;
-use Docker\API\Model\HostConfig;
-use Docker\API\Model\NetworkingConfig;
-
-// Crea una instancia de Docker
-$docker = Docker::create();
-
-// Configura las opciones del contenedor
-$containerConfig = new ContainersCreatePostBody();
-$containerConfig->setImage('ubuntu');
-$containerConfig->setCmd(['echo', 'Hello World']);
-
-// Configura las opciones de red (si es necesario)
-$networkingConfig = new NetworkingConfig();
-
-// Configura las opciones del contenedor en el host
-$hostConfig = new HostConfig();
-$hostConfig->setNetworkMode('bridge');
-$hostConfig->setNetworkingConfig($networkingConfig);
-
-$containerConfig->setHostConfig($hostConfig);
-
-// Crea el contenedor
-$container = $docker->containerCreate($containerConfig);
-
-// Inicia el contenedor
-$docker->containerStart($container->getId());
-
+$output = shell_exec($command);
+echo $output;
 ?>
