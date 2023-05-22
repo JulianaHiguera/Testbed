d0950b8a08c44a8c5aec3891a9637b57f86ef95f
JulianaHiguera <zhiguera@unboque.edu.co>
2023-05-22 17:59:30 -0500

    Notificación erro correjido, sale antes

--- create-container.php
+++ create-container.php
@@ -1,18 +1,32 @@
 <?php
-$sistemaOperativo = $_POST['sistema-operativo'];
-$duracion = $_POST['duracion'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $sistemaOperativo = $_POST['sistema-operativo'];
+    $duracion = $_POST['duracion'];
 
-$password = "juli";
-$command = "echo '$password' | sudo -S ./crar_imagen.sh '$sistemaOperativo' '$duracion'";
+    $password = "juli";
+    $command = "echo '$password' | sudo -S ./crar_imagen.sh '$sistemaOperativo' '$duracion'";
+    $output = shell_exec($command);
 
-// Imprimir mensaje de contenedor creado
-echo "<script>alert('El contenedor $sistemaOperativo ha sido creado por $duracion segundos.');</script>";
+    // Imprimir el código JavaScript para mostrar la alerta
+    echo "<script>
+        var confirmation = confirm('El contenedor $output será creado por $duracion minutos. ¿Deseas continuar?');
+        if (confirmation) {
+            // Si el usuario acepta, enviar el formulario
+            document.getElementById('myForm').submit();
+        } else {
+            // Si el usuario cancela, mostrar un mensaje de cancelación
+            alert('Operación cancelada.');
+        }
+    </script>";
 
-
-$output = shell_exec($command);
-
-// Imprimir mensaje de finalización
-echo "<script>alert('El contenedor $output del sistema operativo $sistemaOperativo a sido eliminado.');</script>";
-
-echo $output;
+    // Detener la ejecución de PHP aquí para esperar la respuesta del usuario
+    exit();
+}
 ?>
+
+<!-- Resto de tu código HTML del formulario -->
+<div class="form-container">
+    <form id="myForm" action="create-container.php" method="post">
+        <!-- ... -->
+    </form>
+</div>
