394848814ce5d1c3ac2392303a0ea75adbf8c7d5
JulianaHiguera <zhiguera@unboque.edu.co>
2023-05-22 16:19:39 -0500

    Prueba del sh solicitando tiempo

--- crar_imagen.sh
+++ crar_imagen.sh
@@ -2,4 +2,25 @@
 
 # Comandos para crear la imagen de docker
 
-sudo docker run $1
+# Nombre de la imagen y del contenedor
+IMAGE_NAME="ubuntu"
+CONTAINER_NAME="imagen_de_prueba"
+
+# Tiempo por defecto (en segundos)
+DEFAULT_TIME=3600  # 1 hora
+
+# Verificar si se especificó un tiempo personalizado
+if [ $# -eq 1 ]; then
+    TIME=$1
+else
+    TIME=$DEFAULT_TIME
+fi
+
+# Crear el contenedor y ejecutarlo en segundo plano
+sudo docker run --detach --name $CONTAINER_NAME $IMAGE_NAME tail -f /dev/null
+
+# Esperar el tiempo especificado
+sleep $TIME
+
+# Detener y eliminar el contenedor
+sudo docker stop $CONTAINER_NAME
