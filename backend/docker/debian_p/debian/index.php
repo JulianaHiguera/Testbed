1d4ecc63b119e9492686cec51e65e2e1e996dec2
JulianaHiguera <zhiguera@unboque.edu.co>
2023-06-02 23:20:38 -0500

    nuevos archivos

---
+++ backend/docker/debian_p/debian/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Debian</title>
+    <style>
+        .container {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $command = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $command = $_POST["command"];
+        $output = shell_exec($command);
+    }
+    ?>
+    <div class="container">
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <input type="text" name="command" placeholder="Escribe tu comando">
+            <button type="submit">Ejecutar</button>
+        </form>
+    </div>
+
+    <div class="container">
+        <strong>Entrada:</strong>
+        <?php echo $command; ?>
+    </div>
+
+    <div class="container">
+        <strong>Salida:</strong>
+        <?php echo $output; ?>
+    </div>
+
+</body>
+
+</html>
