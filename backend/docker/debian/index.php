90a7ac5f6acd9298657b77947685fd851030435a
JulianaHiguera <zhiguera@unboque.edu.co>
2023-06-02 22:14:50 -0500

    Pruebva 3 debian docker

--- backend/docker/debian/index.php
+++ backend/docker/debian/index.php
@@ -1,1 +1,18 @@
-hola mundo desde debian
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Botón de prueba</title>
+</head>
+
+<body>
+    <button onclick="mostrarMensaje()">Haz clic</button>
+
+    <script>
+        function mostrarMensaje() {
+            alert("¡Funciona!");
+        }
+    </script>
+</body>
+
+</html>
