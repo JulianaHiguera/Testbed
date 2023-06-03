bddf6940b37a442ef0b57c30a40b115ea806f56f
JulianaHiguera <zhiguera@unboque.edu.co>
2023-06-02 22:24:33 -0500

    debian mejora pagina para interactuar

--- backend/docker/debian/index.php
+++ backend/docker/debian/index.php
@@ -2,17 +2,27 @@
 <html>
 
 <head>
-    <title>Botón de prueba</title>
+    <title>Debian</title>
 </head>
 
 <body>
-    <button onclick="mostrarMensaje()">Haz clic</button>
+    <?php
+    $command = "";
 
-    <script>
-        function mostrarMensaje() {
-            alert("¡Funciona!");
-        }
-    </script>
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $command = $_POST["command"];
+        $output = shell_exec($command);
+    }
+    ?>
+
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <input type="text" name="command" placeholder="Escribe tu comando">
+        <button type="submit">Ejecutar</button>
+    </form>
+
+    <div>
+        <?php echo $output; ?>
+    </div>
 </body>
 
 </html>
